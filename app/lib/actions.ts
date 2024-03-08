c4725b0a1e03c79429ba6648f8a9e399629af843
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2024-03-08 22:24:18 +0900

    :bug: sign up後にredirectされない問題の修正

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,52 +4,37 @@ import { signIn } from '@/auth';
 import { signOut } from '@/auth';
 import { createUser } from '@/db/user';
 import { AuthError } from 'next-auth';
-import { handleError } from './utils';
 import { DEFAULT_LOGIN_REDIRECT } from '@/route';
-import { NextURL } from 'next/dist/server/web/next-url';
-import { redirect } from 'next/navigation';
 
-export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData,
-  ) {
-    try {
-      const email = formData.get('email') as string;
-      const password = formData.get('password') as string;
-      await signIn('credentials', {email, password, redirectTo: DEFAULT_LOGIN_REDIRECT});
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case 'CredentialsSignin':
-            return 'Invalid credentials.';
-          default:
-            return 'Something went wrong.';
-        }
-      }
-      throw error;
-    }
-  }
-
-export async function signUp(
-    prevState: string | undefined,
-    formData: FormData,
-){
+export async function authenticate(prevState: string | undefined, formData: FormData) {
   try {
-    const name = formData.get('name') as string;
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
-    const result = await createUser(name, email, password);
-    console.log(result)
-    if(!result.isSuccess){
-      return result.error?.message;
+    await signIn('credentials', { email, password, redirectTo: DEFAULT_LOGIN_REDIRECT });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
     }
-  } catch(error) {
-    console.log(error);
-    return 'Failed to sign up.';
-    // throw error;
+    throw error;
   }
-    // await signIn('credentials', {email, password,});
-    // redirect('http://localhost:3000/dashboard')
+}
+
+export async function signUp(prevState: string | undefined, formData: FormData) {
+  const name = formData.get('name') as string;
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+  const result = await createUser(name, email, password);
+  if (result.isSuccess) {
+    // ユーザー作成が成功したら、SignIn処理を行う
+    await authenticate(undefined, formData);
+  } else {
+    return result.error?.message;
+  }
 }
 
 export async function signOutServerAction() {
