2467ae69176601e1018af910f42abfed68c3524b
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2024-03-09 15:28:48 +0900

    :sparkles: サイトのタイトル追加

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { Group, Burger, Button } from '@mantine/core';
+import { Group, Burger, Button, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { MantineLogo } from '@mantinex/mantine-logo';
 import { IconHistory, IconShoppingCart } from '@tabler/icons-react';
 import { signOutServerAction } from '@/app/lib/actions';
 import classes from './Header.module.css';
@@ -13,6 +12,7 @@ import { Menu } from '../Menu/Menu';
 import { DropdownMenuItems } from '../Menu/MenuItems/DropdownMenuItems';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { pacifico } from '@/app/lib/font';
 
 export function Header() {
   const [opened, { toggle }] = useDisclosure(false);
@@ -25,9 +25,9 @@ export function Header() {
     { divider: true },
     { label: 'Account' },
     { text: 'Settings' },
-    { 
-      text: 'Sign out', 
-      color: 'red', 
+    {
+      text: 'Sign out',
+      color: 'red',
       formAction: async () => {
         await signOutServerAction();
       },
@@ -36,15 +36,25 @@ export function Header() {
 
   // ログイン情報取得
   const session = useSession();
-  const user = session.data?.user
-  console.log(session.data)
+  const user = session.data?.user;
+  console.log(session.data);
 
   return (
     <header className={classes.header}>
       <div className={classes.inner}>
         <Group>
           <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-          <MantineLogo size={28} />
+          <div className={classes.icon}>
+            <Text
+              className={`${pacifico.className} antialiased`}
+              size="30"
+              fw={700}
+              variant="gradient"
+              gradient={{ from: 'violet', to: 'indigo', deg: 180 }}
+            >
+              TrendTrail
+            </Text>
+          </div>
         </Group>
 
         <Group className={classes.expand} grow>
@@ -54,25 +64,19 @@ export function Header() {
         <Group>
           <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
             <CartButton />
-            {
-              session.status==='authenticated'&&user
-              ?
+            {session.status === 'authenticated' && user ? (
               <Menu
-              button={<AvatarButton name={user.name?.toString()} />}
-              menuItems={<DropdownMenuItems items={menuItems} />}
-              menuProps={{ position: 'bottom-end' }}
+                button={<AvatarButton name={user.name?.toString()} />}
+                menuItems={<DropdownMenuItems items={menuItems} />}
+                menuProps={{ position: 'bottom-end' }}
               />
-              :
-              <Link href='/signIn'>
-                <Button
-                  variant="gradient"
-                  gradient={{ from: 'violet', to: 'indigo', deg: 120 }}
-                >
+            ) : (
+              <Link href="/signIn">
+                <Button variant="gradient" gradient={{ from: 'violet', to: 'indigo', deg: 120 }}>
                   Sign in
                 </Button>
               </Link>
-            }
-            
+            )}
           </Group>
         </Group>
       </div>
